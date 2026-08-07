753c5997c0ac0c9325e07abf15d8348c764345ac
Samuel <zxv99@proton.me>
2026-08-06 22:55:10 -0300

    main: estructura inicial del sistema de gestion de gimnasio system-gym

---
+++ src/main/java/system_gym/SystemGymApplication.java
@@ -0,0 +1,13 @@
+package system_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/system_gym/SystemGymApplicationTests.java
@@ -0,0 +1,13 @@
+package system_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
